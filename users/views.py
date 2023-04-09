232acc1a349a3cfc3a5cdad04cf13ddcc80b00a4
Preetham Vayigandla <vayigandlapreetham@gmail.com>
2023-04-09 01:16:09 -0700

    Added login, register, users and profile functionality

---
+++ users/views.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from users.forms import RegisterForm
+from users.models import Profile
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Profile.objects.create(user=user)
+            # create a new profile for the user
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
