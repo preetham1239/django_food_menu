045c7ddada53f5c45825d38d2a86e32c6baa5341
Preetham Vayigandla <vayigandlapreetham@gmail.com>
2023-04-18 13:16:48 -0700

    Added Django list views

--- food/urls.py
+++ food/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.IndexClassListView.as_view(), name='index'),
     path('<int:pk>/', views.FoodItemDetailView.as_view(), name='detail'),
     path('fooditem/', views.items, name='items'),
-    path('add/', views.create_item, name='create_item'),
+    path('add/', views.FoodItemCreateView.as_view(), name='create_item'),
     path('update/<int:id>/', views.update_item, name='update_item'),
     path('delete/<int:id>/', views.delete_item, name='delete_item'),
 ]
--- food/views.py
+++ food/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.utils.decorators import method_decorator
 from django.views import generic
 
 from .forms import FoodItemForm
@@ -27,12 +29,25 @@ def detail(request, item_id):
     }
     return render(request, 'food/detail.html', context)
 
-def create_item(request):
-    form = FoodItemForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect('food:index')
-    return render(request, 'food/item_form.html', {'form': form})
+
+class FoodItemCreateView(generic.CreateView):
+    model = FoodItem
+    fields = ['item_name', 'item_desc', 'item_price', 'item_image']
+    template_name = 'food/item_form.html'
+
+    # def form_valid(self, form):
+    #     print(1000000000000000000000, self.request.user)
+    #     form.instance.user = self.request.user
+    #     print(1000000000000000000000, form)
+    #     return super().form_valid(form)
+    #
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def form_valid(self, form):
+        form.instance.user_name = self.request.user
+        return super().form_valid(form)
 
 def update_item(request, id):
     item = FoodItem.objects.get(pk=id)
